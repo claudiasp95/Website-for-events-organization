3cf258c5ff07d1ae449386be505119fd90035327
Spinu Claudia <cojocaru.claudia995@gmail.com>
2021-03-07 11:42:05 +0200

    Initial setup of project

---
+++ websiteforeventsorg/src/main/java/com/sda/websiteforeventsorg/WebsiteforeventsorgApplication.java
@@ -0,0 +1,13 @@
+package com.sda.websiteforeventsorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteforeventsorgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteforeventsorgApplication.class, args);
+	}
+
+}
---
+++ websiteforeventsorg/src/test/java/com/sda/websiteforeventsorg/WebsiteforeventsorgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.websiteforeventsorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteforeventsorgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
